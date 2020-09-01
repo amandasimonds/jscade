bf21d72a3e48541184099a3b077abdd3f8ecc710
Amanda Tree <amandasimonds9@gmail.com>
2020-09-01 13:17:25 -0600

    crash logic

--- games/rocket/rocket.js
+++ games/rocket/rocket.js
@@ -9,7 +9,9 @@ var gamePiece
 function startGame(){
     gameArea.start();
     //define our game piece with the component object constructor
-    gamePiece = new component(30, 30, "red", 10, 120)
+    gamePiece = new component(30, 30, "red", 10, 120, false),
+    obstacle = new component(10, 200, "green", 100, 120, false)
+    console.log(gamePiece.x, obstacle.x)
 }
 
 //define our game area
@@ -53,6 +55,11 @@ var gameArea = {
     //clears the game area using the clearRect method
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    },
+
+    //tells the game to stop
+    stop : function() {
+        clearInterval(this.interval)
     }
 }
 
@@ -72,6 +79,7 @@ function component(width, height, color, x, y) {
         //define color
         ctx.fillStyle = color;
         //use the canvas method to create a rectangle with the x position, y position, width, and height that you put in your component
+        //this clears the canvas
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
@@ -80,13 +88,50 @@ function component(width, height, color, x, y) {
         this.x += this.speedX;
         this.y += this.speedY
     }
+
+    this.crashWith = function(otherobj){
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if (
+            (mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)
+        ){
+            crash = false
+        }
+        return crash
+    }
 }
 
 //this function will update our game area
 function updateGameArea(){
+    //if crashes with obstacle, end the game
+    if (gamePiece.crashWith(obstacle)) {
+        gameArea.stop();
+        console.log(gamePiece)
+    } else {
+        //clear the game area so that we do not see the game piece in its last position
+        gameArea.clear();
+        obstacle.update();
+        //move the position of gamePiece every time the game area updates (every 20 milliseconds)
+        gamePiece.newPos();
 
-    //first, clear the game area so that we do not see the game piece in its last position
-    gameArea.clear();
+        //this is for the mouse control. it will not work with the arrow controls too, so you must choose which one to use
+        // if (gameArea.x && gameArea.y){
+        //     gamePiece.x = gameArea.x;
+        //     gamePiece.y = gameArea.y;
+        // }
+
+        gamePiece.update();
+    }
 
     //gamePiece starts at a speed of 0
     gamePiece.speedX = 0;
@@ -97,17 +142,6 @@ function updateGameArea(){
     if (gameArea.keys && gameArea.keys[39]) {gamePiece.speedX = 1; }
     if (gameArea.keys && gameArea.keys[38]) {gamePiece.speedY = -1; }
     if (gameArea.keys && gameArea.keys[40]) {gamePiece.speedY = 1; }
-
-    //move the position of gamePiece every time the game area updates (every 20 milliseconds)
-    gamePiece.newPos();
-
-    //this is for the mouse control. it will not work with the arrow controls too, so you must choose which one to use
-    // if (gameArea.x && gameArea.y){
-    //     gamePiece.x = gameArea.x;
-    //     gamePiece.y = gameArea.y;
-    // }
-
-    gamePiece.update();
 }
 
 function moveUp(){
